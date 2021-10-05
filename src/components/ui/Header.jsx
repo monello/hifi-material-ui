9f141166ddb89d455e4322134161aaac7256e530
Morne Louw <morne.louw@saongroup.com>
2021-10-05 20:24:23 +0200

    Added the initial AppBar as the site Header

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AppBar, Toolbar } from "@mui/material";
+
+const Header = (props) => {
+  return (
+    <AppBar position="fixed">
+      <Toolbar>Arc Developement</Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
