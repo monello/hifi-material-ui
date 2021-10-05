e2cc9dde59f70e3cbf50f8f9687ffc4d8634a3e9
Morne Louw <morne.louw@saongroup.com>
2021-10-05 21:52:45 +0200

    Scroll-Elevation quick cleanup as per comments in the code

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -4,13 +4,9 @@ import { AppBar, Toolbar, useScrollTrigger } from "@mui/material";
 
 function ElevationScroll(props) {
   const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 0,
-    target: window ? window() : undefined,
   });
 
   return React.cloneElement(children, {
@@ -20,11 +16,6 @@ function ElevationScroll(props) {
 
 ElevationScroll.propTypes = {
   children: PropTypes.element.isRequired,
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
 };
 
 const Header = (props) => {
