d1f98d72b603648be6a46100b1954b7bb6983ab4
Morne Louw <morne.louw@saongroup.com>
2021-10-05 22:52:51 +0200

    Custom Theme - The Palette object (configurable theme variables)

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,7 +1,23 @@
 import { createTheme } from '@mui/material/styles'
 
+const arcBlue = "#0b72b9"
+const arcOrange = "#FFBA60"
+
 // Create a new instance of the default MUI theme.
 // Then provide it an object where we override special them config variables, or add some new ones
-const theme = createTheme({})
+const theme = createTheme({
+    palette: {
+        common: {
+            blue: arcBlue,
+            orange: arcOrange
+        },
+        primary: {
+            main: arcBlue
+        },
+        secondary: {
+            main: arcOrange
+        }
+    }
+})
 
 export default theme
