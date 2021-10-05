324a3c0becee521a8e403de6f2fb09f5442b1682
Morne Louw <morne.louw@saongroup.com>
2021-10-05 21:56:11 +0200

    Deleted the Temp Lorem Ipsum component again

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,10 @@
 import Header from "./ui/Header";
 
-// Temp import, just for testing
-import { LoremIpsum } from "react-lorem-ipsum";
-
 function App() {
   return (
     <div className="App">
       <Header />
-      <LoremIpsum
-        p={50}
-        avgWordsPerSentence={10}
-        avgSentencesPerParagraph={10}
-      />
+      Hello World!
     </div>
   );
 }
