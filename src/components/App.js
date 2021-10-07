51c8ff4f5a747dd3ab09abe030f7bac6e182ccd5
Morne Louw <morne.louw@saongroup.com>
2021-10-07 10:45:26 +0200

    fixed a typo in the exact prop of all the Route components

--- src/components/App.js
+++ src/components/App.js
@@ -10,15 +10,15 @@ function App() {
       <BrowserRouter>
         <Header />
         <Switch>
-          <Route extact path="/" component={() => <div>Home Page</div>}></Route>
-          <Route extact path="/services" component={() => <div>Services Page</div>}></Route>
-          <Route extact path="/customsoftware" component={() => <div>Custom Software Page</div>}></Route>
-          <Route extact path="/mobileapps" component={() => <div>Mobile Apps Page</div>}></Route>
-          <Route extact path="/websites" component={() => <div>Websites Page</div>}></Route>
-          <Route extact path="/revolution" component={() => <div>The Revolution Page</div>}></Route>
-          <Route extact path="/about" component={() => <div>About Us Page</div>}></Route>
-          <Route extact path="/contact" component={() => <div>Contact Us Page</div>}></Route>
-          <Route extact path="/estimate" component={() => <div>Free Estimates Page</div>}></Route>
+          <Route exact path="/" component={() => <div>Home Page</div>}></Route>
+          <Route exact path="/services" component={() => <div>Services Page</div>}></Route>
+          <Route exact path="/customsoftware" component={() => <div>Custom Software Page</div>}></Route>
+          <Route exact path="/mobileapps" component={() => <div>Mobile Apps Page</div>}></Route>
+          <Route exact path="/websites" component={() => <div>Websites Page</div>}></Route>
+          <Route exact path="/revolution" component={() => <div>The Revolution Page</div>}></Route>
+          <Route exact path="/about" component={() => <div>About Us Page</div>}></Route>
+          <Route exact path="/contact" component={() => <div>Contact Us Page</div>}></Route>
+          <Route exact path="/estimate" component={() => <div>Free Estimates Page</div>}></Route>
         </Switch>
       </BrowserRouter>
     </ThemeProvider>
