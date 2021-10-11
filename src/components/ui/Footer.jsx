8228ae4df7868515dc1f45804fb96b9e8502d6c9
Morne Louw <morne.louw@saongroup.com>
2021-10-11 21:19:28 +0200

    Added a basic starter grid to the Footer

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { makeStyles } from "@mui/styles";
+import { Grid } from "@mui/material";
 
 import footerAdornment from "../../assets/Footer Adornment.svg";
 
@@ -20,6 +21,15 @@ const useStyles = makeStyles((theme) => ({
       width: "15em",
     },
   },
+  mainContainer: {
+    position: "absolute",
+  },
+  link: {
+    color: "white",
+    fontFamily: "Arial",
+    fontSize: "0.75rem",
+    fontWeight: "bold",
+  },
 }));
 
 const Footer = (props) => {
@@ -27,6 +37,11 @@ const Footer = (props) => {
 
   return (
     <footer className={classes.footer}>
+      <Grid container className={classes.mainContainer}>
+        <Grid item className={classes.link}>
+          Hone
+        </Grid>
+      </Grid>
       <img
         alt="black decorative slash"
         src={footerAdornment}
