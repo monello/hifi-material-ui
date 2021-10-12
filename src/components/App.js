9fa22d0211f4304ddf25ec77a3c929f1cb21a8e0
Morne Louw <morne.louw@saongroup.com>
2021-10-12 17:01:19 +0200

    Added some Temp Lorem Ipsum text on each page to push the Footer down

--- src/components/App.js
+++ src/components/App.js
@@ -6,26 +6,123 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from "./ui/Theme";
 
+import { LoremIpsum } from "react-lorem-ipsum";
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Header />
         <Switch>
-          <Route exact path="/" component={() => <div style={{ height: "900px" }}>Home Page<p>Foo</p></div>}></Route>
-          <Route exact path="/services" component={() => <div>Services Page</div>}></Route>
-          <Route exact path="/customsoftware" component={() => <div>Custom Software Page</div>}></Route>
-          <Route exact path="/mobileapps" component={() => <div>Mobile Apps Page</div>}></Route>
-          <Route exact path="/websites" component={() => <div>Websites Page</div>}></Route>
-          <Route exact path="/revolution" component={() => <div>The Revolution Page</div>}></Route>
-          <Route exact path="/about" component={() => <div>About Us Page</div>}></Route>
-          <Route exact path="/contact" component={() => <div>Contact Us Page</div>}></Route>
-          <Route exact path="/estimate" component={() => <div>Free Estimates Page</div>}></Route>
+          <Route exact path="/" component={TempHomePage}></Route>
+          <Route exact path="/services" component={TempServicesPage}></Route>
+          <Route exact path="/customsoftware" component={TempCustomSoftwarePage}></Route>
+          <Route exact path="/mobileapps" component={TempMobileAppsPage}></Route>
+          <Route exact path="/websites" component={TempWebsitesPage}></Route>
+          <Route exact path="/revolution" component={TempRevolutionPage}></Route>
+          <Route exact path="/about" component={TempAboutPage}></Route>
+          <Route exact path="/contact" component={TempContactPage}></Route>
+          <Route exact path="/estimate" component={TempFreeEstimatestPage}></Route>
         </Switch>
         <Footer />
       </BrowserRouter>
     </ThemeProvider>
   );
 }
+
+const TempHomePage = () => (
+  <div>
+    <h1>Home Page</h1>
+    <LoremIpsum
+      p={6}
+      avgWordsPerSentence={10}
+      avgSentencesPerParagraph={10}
+    />
+  </div>
+);
+
+const TempServicesPage = () => (
+  <div>
+    <h1>Services Page</h1>
+    <LoremIpsum
+      p={6}
+      avgWordsPerSentence={10}
+      avgSentencesPerParagraph={10}
+    />
+  </div>
+);
+
+const TempCustomSoftwarePage = () => (
+  <div>
+    <h1>Custom Software Page</h1>
+    <LoremIpsum
+      p={6}
+      avgWordsPerSentence={10}
+      avgSentencesPerParagraph={10}
+    />
+  </div>
+);
+const TempMobileAppsPage = () => (
+  <div>
+    <h1>Modile Apps Page</h1>
+    <LoremIpsum
+      p={6}
+      avgWordsPerSentence={10}
+      avgSentencesPerParagraph={10}
+    />
+  </div>
+);
+const TempWebsitesPage = () => (
+  <div>
+    <h1>Websites Page</h1>
+    <LoremIpsum
+      p={6}
+      avgWordsPerSentence={10}
+      avgSentencesPerParagraph={10}
+    />
+  </div>
+);
+const TempRevolutionPage = () => (
+  <div>
+    <h1>The Revolution Page</h1>
+    <LoremIpsum
+      p={6}
+      avgWordsPerSentence={10}
+      avgSentencesPerParagraph={10}
+    />
+  </div>
+);
+const TempAboutPage = () => (
+  <div>
+    <h1>About Page</h1>
+    <LoremIpsum
+      p={6}
+      avgWordsPerSentence={10}
+      avgSentencesPerParagraph={10}
+    />
+  </div>
+);
+
+const TempContactPage = () => (
+  <div>
+    <h1>Contact Page</h1>
+    <LoremIpsum
+      p={6}
+      avgWordsPerSentence={10}
+      avgSentencesPerParagraph={10}
+    />
+  </div>
+);
+
+const TempFreeEstimatestPage = () => (
+  <div>
+    <h1>Free Estimates Page</h1>
+    <LoremIpsum
+      p={6}
+      avgWordsPerSentence={10}
+      avgSentencesPerParagraph={10}
+    />
+  </div>
+);
 
 export default App;
