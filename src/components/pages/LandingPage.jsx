46df6be592dbf20f1d145f7767d2fef7fe3f850b
Morne Louw <morne.louw@saongroup.com>
2021-10-13 20:37:13 +0200

    Added Responsive layout to the Hero Block

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -3,8 +3,20 @@ import { Grid, Button, Typography } from "@mui/material";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 import cpuImage from "../../assets/Landing-Page-CPU.png";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  animation: {
+    maxWidth: "50em !important",
+    minWidth: "21em !important",
+    marginTop: "2em !important",
+    marginLeft: "10% !important",
+  },
+}));
 
 const LandingPage = () => {
+  const classes = useStyles();
+
   return (
     <Grid container direction="column">
       <Grid item>
@@ -14,7 +26,7 @@ const LandingPage = () => {
           justifyContent="flex-end"
           alignItems="center"
         >
-          <Grid item>
+          <Grid item sm>
             <Typography variant="h2" align="center">
               Bringing West Coast Technology
               <br /> to the Midwest
@@ -33,7 +45,7 @@ const LandingPage = () => {
               </Grid>
             </Grid>
           </Grid>
-          <Grid item>
+          <Grid item sm className={classes.animation}>
             <img alt="Landing Page West Coast Technology" src={cpuImage} />
           </Grid>
         </Grid>
